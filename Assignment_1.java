c8428e135d4c7e3e7702e3806a506c37962f5282
GauravD98 <gauravdebnath1998@gmail.com>
2023-08-31 23:42:25 +0530

    Assignment 1

---
+++ Assignment_1.java
@@ -0,0 +1,14 @@
+
+public class Assignment_1 {
+
+	public static void main(String[] args) {
+
+		int numbers[] = {1,2,3,4};
+		
+		System.out.println(numbers[0]);
+		System.out.println(numbers[2]);
+		System.out.println(numbers[4]);
+			
+	}
+
+}
