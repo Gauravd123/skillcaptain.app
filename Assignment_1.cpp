abb7230f0e75a935558643e01f7e62920e2a1c04
GauravD98 <gauravdebnath1998@gmail.com>
2023-08-31 23:51:45 +0530

    Create Assignment_1.cpp

---
+++ Assignment_1.cpp
@@ -0,0 +1,9 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int numbers[] = {1,2,3,4,5};
+    cout<<numbers[0]<<" "<<numbers[2]<<" "<<numbers[4];
+    
+    return 0;
+}
