7618383edcd186fbd31a49e066cbd87d04fde5a4
GauravD98 <gauravdebnath1998@gmail.com>
2023-08-31 23:49:41 +0530

    Update Assignment_1.java

--- Assignment_1.java
+++ Assignment_1.java
@@ -3,7 +3,7 @@ public class Assignment_1 {
 
 	public static void main(String[] args) {
 
-		int numbers[] = {1,2,3,4};
+		int numbers[] = {1,2,3,4,5};
 		
 		System.out.println(numbers[0]);
 		System.out.println(numbers[2]);
